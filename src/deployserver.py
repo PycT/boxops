667bd88e53c9a455dc9a4bb4e6ec1c4da5fdd9c3
PycT <rustem.zakiev@gmail.com>
2020-12-08 20:39:20 +0300

    now it's alive

--- src/deployserver.py
+++ src/deployserver.py
@@ -4,8 +4,12 @@ import os
 from rhythmic import Logger
 from yaml import safe_load as yaml_load
 
-# deploy_logger = Logger(log_filename_postfix = "deploy", path_to_log = ".")
-# deploy_logger.writeDown("=========== Deployer Service Start ==============")
+path_to_log = os.getcwd()
+blocker = path_to_log + "/deploy.blk"
+
+
+deploy_logger = Logger(log_filename_postfix = "deploy", path_to_log = path_to_log)
+deploy_logger.writeDown("=========== Deployer Service Start ==============")
 
 def get_deploy_configuration():
 
@@ -14,10 +18,89 @@ def get_deploy_configuration():
 
     return deploy_configuration
 
-deploy_configuration = get_deploy_configuration()
+def get_steps(deploy_configuration, the_stand):
+
+    steps = {}
+    for stand in deploy_configuration:
+        if stand["stand"]["name"] == the_stand:
+            steps = stand["stand"]["steps"]
+            break
+
+    return steps
+
 
 def execute_step(step):
-    pass  # TODO: step execution
+    """
+        - step:
+              description: "Cleaning"
+              path: "/var/www"
+              user: ""
+              command: "rm -rf mydir"
+    """
+    operation_output = ""
+
+    try:
+        if len(step["description"]) > 0:
+            deploy_logger.writeDown(step["description"])
+            operation_output += step["description"] + "\n\n"
+        else:
+            deploy_logger.writeDown("step")
+            operation_output += " step\n "
+    except Exception as e:
+        print(e)
+#         operation_output += repr(e) + "\n\n"
+
+    the_user = ""
+
+    try:
+        the_user = step["user"]
+    except Exception as e:
+        print(e)
+        # operation_output += repr(e) + "\n\n"
+
+
+    user_path = ""
+
+    try:
+        if len(step["path"]) > 0:
+            if len(the_user)== 0:
+                os.chdir(step["path"])
+            else:
+                user_path = step["path"]
+    except Exception as e:
+        print(e)
+        # operation_output += repr(e) + "\n\n"
+
+    try:
+        if len(step["command"]) > 0:
+            execution_output = ""
+            if len(the_user) > 0:
+                if len(user_path) > 0:
+                    user_command = "cd {}; ".format(user_path) + step["command"]
+                else:
+                    user_command = step["command"]
+		
+                process_result = subprocess.run(\
+                                    ["runuser", "-l", the_user, "-c", user_command],\
+                                    stdout = subprocess.PIPE, stderr = subprocess.STDOUT\
+                                 )
+            else:
+                process_result = subprocess.run(\
+                                    step["command"].split(" "),\
+                                    stdout = subprocess.PIPE, stderr = subprocess.STDOUT\
+                                 )
+            execution_output += process_result.stdout.decode()
+            deploy_logger.writeDown(execution_output)
+            operation_output += execution_output + "\n<br>\n"
+
+    except Exception as e:
+        print(e)
+        # operation_output += repr(e) + "\n\n"
+
+    return operation_output + "\n --- \n"
+
+
+deploy_configuration = get_deploy_configuration()
 
 app = Flask(__name__)
 
@@ -25,99 +108,46 @@ app = Flask(__name__)
 @app.route("/index", methods=["POST", "GET"])
 def index():
 
-    print(request.method)
+    deploy_logger.writeDown(request.method)
     context = {
-        "stands": []
+        "stands": [],
+        "deploy_in_progress": False
     }
 
     for stand in deploy_configuration:
         context["stands"].append(stand["stand"]["name"])
 
+    if os.path.exists(blocker):
+        context["deploy_in_progress"] = True
+        return render_template("deploy.html", context=context)
+
     if request.method == "POST":
+
+        with open(blocker, "w") as blockerfile:
+            blockerfile.write(".")
+
         the_stand = request.values["the_stand"]
         the_branch = request.values["the_branch"]
 
-        print(the_stand)
-        print(the_branch)
+        deploy_logger.writeDown(the_stand)
+        deploy_logger.writeDown(the_branch)
+
+        scenario = get_steps(deploy_configuration, the_stand)
+        scenario_output = ""
+
+        for the_step in scenario:
+            if the_step["step"]["description"] == "Git branch":
+                the_step["step"]["command"] = "git checkout {}".format(the_branch)
+
+            scenario_output += execute_step(the_step["step"])
+
+        context["the_stand"] = the_stand
+        context["the_branch"] = the_branch
+        context["scenario_output"] = scenario_output.replace("\n","<br>")
+
+        os.remove(blocker)
 
     return render_template("deploy.html", context=context)
-
-
-
-# def index():
-#
-#     os.chdir(local_repo_path)
-#     process_result = subprocess.run(["git", "log", "-10"], stdout = subprocess.PIPE, stderr = subprocess.STDOUT);
-#
-#     git_log = "<h2>git log:</h2>\n{}".format(process_result.stdout.decode());
-#
-#     return render_text(git_log);
-
-# @app.route("/deploy", methods = ["POST"])
-# def deploy():
-#
-#     deploy_stand = request.values["deploy_stand"];
-#     deploy_branch = request.values["deploy_branch"];
-#     deploy_path = deploy_paths[deploy_stand];
-#
-#     deploy_blocker_path = "{}/block.dpl".format(local_log_path);
-#
-#     if os.path.exists(deploy_blocker_path):
-#         deploy_logger.writeDown("Deploy in progress, try later.");
-#         return render_text("Deploy in progress, try again 5 minutes later.")
-#
-#     with open(deploy_blocker_path, "a") as deploy_blocker:
-#         deploy_blocker.write("\n");
-#
-#     deploy_logger.writeDown("Deploy branch: {}".format(deploy_branch));
-#     deploy_logger.writeDown("Deploy path: {}".format(deploy_path));
-#
-#     stdout_text = "Deploy branch: {}\nDeploy path: {}\n\n".format(deploy_branch, deploy_path);
-#
-#     deploy_steps[0]["call"][2] = deploy_path;
-#     deploy_steps[1]["call"][1] = deploy_path;
-#     deploy_steps[3]["call"][2] = deploy_branch;
-#     deploy_steps[5]["call"][3] = deploy_path;
-#     deploy_steps[6]["dir"] = deploy_path;
-#     # deploy_steps are defined in config.py
-#
-#     deploy_logger.writeDown("Proceeding...");
-#
-#     for info_step in info_steps:
-#
-#         if info_step["dir"] != None:
-#             os.chdir(info_step["dir"]);
-#
-#         process_result = subprocess.run(info_step["call"], stdout = subprocess.PIPE, stderr = subprocess.STDOUT);
-#         op_stdout = process_result.stdout.decode();
-#         output_text = "\n{} \n\n{}".format(info_step["name"], op_stdout);
-#         deploy_logger.writeDown(output_text);
-#         stdout_text += output_text;
-#         with open(deploy_blocker_path, "a") as deploy_blocker:
-#             deploy_blocker.write(output_text);
-#
-#     for deploy_step in deploy_steps:
-#
-#         if deploy_step["dir"] != None:
-#             os.chdir(deploy_step["dir"]);
-#
-#         process_result = subprocess.run(deploy_step["call"], stdout = subprocess.PIPE, stderr = subprocess.STDOUT);
-#
-#         op_stdout = process_result.stdout.decode();
-#         output_text = "{} \n{}".format(deploy_step["name"], op_stdout);
-#
-#         deploy_logger.writeDown(output_text);
-#         with open(deploy_blocker_path, "a") as deploy_blocker:
-#             deploy_blocker.write(output_text);
-#         stdout_text += output_text;
-#
-#
-#     stdout_text += "\nDone.";
-#     deploy_logger.writeDown("Done.");
-#
-#     os.remove(deploy_blocker_path);
-#
-#     return render_text(stdout_text);
 
 
 def main():
